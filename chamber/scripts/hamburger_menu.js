b4bd43b4d3f85c5c9da77c3ba8dc795bfa45ea58
Rai Katsuragawa <106378196+katsu-rai@users.noreply.github.com>
2023-10-18 14:55:05 -0600

    fixed the x button

--- chamber/scripts/hamburger_menu.js
+++ chamber/scripts/hamburger_menu.js
@@ -1,12 +1,15 @@
 const mainnav = document.querySelector("#navigation");
 const hambutton = document.querySelector("#menu");
+const closebutton = document.querySelector("#close-button")
 
 hambutton.addEventListener("click", () => {
   mainnav.classList.toggle("show");
+  closebutton.classList.toggle("show")
   hambutton.classList.toggle("show");
 });
 
 mainnav.addEventListener("click", () => {
   mainnav.classList.toggle("show");
+  closebutton.classList.toggle("show")
   hambutton.classList.toggle("show");
 });
