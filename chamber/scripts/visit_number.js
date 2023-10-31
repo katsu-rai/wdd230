e0130065d12148008eca5e7cc491999b60860c5f
Rai Katsuragawa <106378196+katsu-rai@users.noreply.github.com>
2023-10-31 08:51:08 -0600

    updated webpage

--- chamber/scripts/visit_number.js
+++ chamber/scripts/visit_number.js
@@ -1,15 +1,39 @@
 const visitsDisplay = document.querySelector(".visits");
 
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+const msToDays = 84600000;
 
-
-
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+function daysSinceLastVisit() {
+    // Get the last visit date from local storage
+    var lastVisit = localStorage.getItem('lastVisit');
+  
+    if (lastVisit) {
+      // Convert the stored date string to a Date object
+      var lastVisitDate = new Date(lastVisit);
+      
+      // Get the current date
+      var currentDate = new Date();
+      
+      // Calculate the time difference in milliseconds
+      var timeDifference = currentDate - lastVisitDate;
+      // Calculate the number of days
+      var daysDifference = Math.floor(timeDifference / msToDays);
+      if (daysDifference < 1) {
+          visitsDisplay.textContent = 'Back so soon! Awesome!'
+        }
+        else {
+            visitsDisplay.textContent = 'You last visited ' + daysDifference + ' days ago.'
+        }
+        
+    } else {
+        // If there's no stored last visit date, it's the user's first visit
+        visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`
+    }
 }
+  
+  // Call the function and display the result
+  daysSinceLastVisit();
 
-numVisits++;
-
-localStorage.setItem("numVisits-ls", numVisits);
+  // Update the last visit date in local storage to the current date
+  var currentDate = new Date();
+  localStorage.setItem('lastVisit', currentDate);
+  
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,16 +1,15 @@
-let temp = parseInt(document.querySelector("#temperature").innerHTML)
-let windSpeed = parseFloat(document.querySelector("#wind-speed").innerHTML)
+let temp = parseInt(document.querySelector(".temperature").innerHTML)
+let windSpeed = parseFloat(document.querySelector(".wind-speed").innerHTML)
 
 
 console.log(temp)
-console.log(windSpeed)
 
 let windChill = 35.74 + 0.6215 * temp - 35.75 * windSpeed ** 0.16 + 0.4275 * temp * windSpeed ** 0.16
 windChill = windChill.toFixed(2)
 
-const IdWindChill = document.querySelector("#wind-chill")
+const IdWindChill = document.querySelector(".wind-chill")
 
-if (temperature<= 50 && windSpeed > 3.0) {
+if (temp <= 50 && windSpeed > 3.0) {
     IdWindChill.innerHTML = "N/A"
 }
 else{
