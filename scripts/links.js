34ca86c0fa8579f8cd6dde1555c73a6ca8960e76
Rai Katsuragawa <106378196+katsu-rai@users.noreply.github.com>
2023-11-10 00:14:50 -0700

    made a dynamic lesson lists

--- scripts/links.js
+++ scripts/links.js
@@ -17,27 +17,37 @@ async function getLinks() {
 }
 
 const displayLinks = (data) => {
-    data.lessons.forEach((eachLesson) => {console.log(eachLesson.lesson)
-    
-    //   let lesson = document.createElement('li');
-    //   let lessonNumber = eachLesson.lesson
-    //   console(lessonNumber)
-    //   let lessonTitle = document.createElement('__'); // fill in the blank
-    //   let portrait = document.createElement('img');
+    data.lessons.forEach((eachLesson) => {
+
+      let lesson = document.createElement('li');
+      let lessonNumber = eachLesson.lesson;
+      lesson.innerText = lessonNumber;
+
+      eachLesson.links.forEach((link) => {
+        let aTagLink = document.createElement('a')
+        aTagLink.setAttribute('href', link.url)
+        aTagLink.innerText = link.title
+
+        lesson.appendChild(aTagLink)
+      })
+
+      const lessons = document.querySelector('#lessons')
+      lessons.appendChild(lesson)
   
-    //   // Build the h2 content out to show the prophet's full name
-    //   fullName.textContent = `${prophet._____} ____________`; // fill in the blank
-    //   // Build the image portrait by setting all the relevant attributes
-    //   portrait.setAttribute('src', prophet.imageurl);
-    //   portrait.setAttribute('alt', `Portrait of ${prophet.____} ______________`); // fill in the blank
-    //   portrait.setAttribute('loading', 'lazy');
-    //   portrait.setAttribute('width', '340');
-    //   portrait.setAttribute('height', '440');
-  
-    //   // Append the section(card) with the created elements
-    //   card.appendChild(_______); //fill in the blank
-    //   card.appendChild(portrait);
-  
-    //   cards.appendChild(card);
-    }); // end of arrow function and forEach loop
+    });
 }
+
+//   // Build the h2 content out to show the prophet's full name
+//   fullName.textContent = `${prophet._____} ____________`; // fill in the blank
+//   // Build the image portrait by setting all the relevant attributes
+//   portrait.setAttribute('src', prophet.imageurl);
+//   portrait.setAttribute('alt', `Portrait of ${prophet.____} ______________`); // fill in the blank
+//   portrait.setAttribute('loading', 'lazy');
+//   portrait.setAttribute('width', '340');
+//   portrait.setAttribute('height', '440');
+
+//   // Append the section(card) with the created elements
+//   card.appendChild(_______); //fill in the blank
+//   card.appendChild(portrait);
+
+//   cards.appendChild(card);
