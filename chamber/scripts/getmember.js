cabdac217b1618e6a62781ed3a2af5ebd963b88d
Rai Katsuragawa <106378196+katsu-rai@users.noreply.github.com>
2023-11-13 20:49:43 -0700

    updated the image links in json file

--- chamber/scripts/getmember.js
+++ chamber/scripts/getmember.js
@@ -1,7 +1,10 @@
+const membersURL = "https://katsu-rai.github.io/wdd230/chamber/data/members.json";
+const displayMembers = document.querySelector("#members")
+
 getMembers();
 
 async function getMembers() {
-    const response = await fetch("../data/members.json");
+    const response = await fetch("https://katsu-rai.github.io/wdd230/chamber/data/members.json");
     const data = await response.json();
     displayLinks(data.members);
 }
@@ -10,22 +13,34 @@ const displayLinks = (data) => {
     data.forEach((member) => {
 
       let card = document.createElement('section');
+      let nameDisplay =document.createElement('p');
+      let membershipDisplay =document.createElement('p');
+      let phoneNumberDisplay =document.createElement('p');
+      let websiteURLDisplay =document.createElement('a');
+      let imgTag = document.createElement('img');
+
+      let imageLink = member.image;
       let name = member.name;
+      let membership = member.membership;
       let phoneNumber = member.phoneNumber;
       let websiteURL = member.websiteURL;
-      let imageLink = member.image;
-      let membership = member.membership;
 
-      console.log(name)
-      console.log(phoneNumber)
-      console.log(websiteURL)
-      console.log(imageLink)
-      console.log(membership)
+        imgTag.setAttribute('src', imageLink);
+        imgTag.setAttribute('alt', `Profile of ${name}`);
+        imgTag.setAttribute('loading', 'lazy');
+        card.appendChild(imgTag);
 
-    //   lesson.innerText = name;
+      nameDisplay.innerText = name;
+      card.appendChild(nameDisplay);
+      membershipDisplay.innerText = membership;
+      card.appendChild(membershipDisplay);
+      phoneNumberDisplay.innerText = phoneNumber;
+      card.appendChild(phoneNumberDisplay);
+      websiteURLDisplay.innerText = websiteURL;
+      websiteURLDisplay.setAttribute("href", websiteURL)
+      card.appendChild(websiteURLDisplay);
 
-    //   const lessons = document.querySelector('#lessons')
-    //   lessons.appendChild(lesson)
-  
+        displayMembers.appendChild(card);
+
     });
 }
