593793349182618fa3275b08514ff3ba5059a15d
Rai Katsuragawa <106378196+katsu-rai@users.noreply.github.com>
2023-11-27 08:18:10 -0700

    created get spotlight function

--- chamber/scripts/getspotlight.js
+++ chamber/scripts/getspotlight.js
@@ -1,46 +1,55 @@
 const membersURL = "https://katsu-rai.github.io/wdd230/chamber/data/members.json";
 const displayMembers = document.querySelector("#members")
 
-getMembers();
+getSpotlight();
 
-async function getMembers() {
+async function getSpotlight() {
     const response = await fetch("https://katsu-rai.github.io/wdd230/chamber/data/members.json");
     const data = await response.json();
     displayLinks(data.members);
 }
 
 const displayLinks = (data) => {
-    data.forEach((member) => {
+    const spotlight1 = document.querySelector("#spotlight1")
+    const spotlightCompany1 = document.querySelector("#spotlight-company-name1")
+    const spotlight2 = document.querySelector("#spotlight2")
+    const spotlightCompany2 = document.querySelector("#spotlight-company-name2")
 
-      let card = document.createElement('section');
-      let nameDisplay =document.createElement('p');
-      let membershipDisplay =document.createElement('p');
-      let phoneNumberDisplay =document.createElement('p');
-      let websiteURLDisplay =document.createElement('a');
-      let imgTag = document.createElement('img');
+    const filteredData = data.filter(user => user.membership === 'Gold' || user.membership === 'Silver');
 
-      let imageLink = member.image;
-      let name = member.name;
-      let membership = member.membership;
-      let phoneNumber = member.phoneNumber;
-      let websiteURL = member.websiteURL;
+    let randomUsers = getRandomElements(filteredData, 2);
 
-        imgTag.setAttribute('src', imageLink);
-        imgTag.setAttribute('alt', `Profile of ${name}`);
-        imgTag.setAttribute('loading', 'lazy');
-        card.appendChild(imgTag);
+    const imgURL1 = randomUsers[0].image;
+    const companyName1 = randomUsers[0].name;
+    
+    const imgURL2 = randomUsers[1].image;
+    const companyName2 = randomUsers[1].name;
 
-      nameDisplay.innerText = name;
-      card.appendChild(nameDisplay);
-      membershipDisplay.innerText = membership;
-      card.appendChild(membershipDisplay);
-      phoneNumberDisplay.innerText = phoneNumber;
-      card.appendChild(phoneNumberDisplay);
-      websiteURLDisplay.innerText = websiteURL;
-      websiteURLDisplay.setAttribute("href", websiteURL)
-      card.appendChild(websiteURLDisplay);
+    let imgTag1 = document.createElement('img');
+    let imgTag2 = document.createElement('img');
+    
+    imgTag1.setAttribute('src', imgURL1);
+    imgTag1.setAttribute('alt', `Profile of ${companyName1}`);
+    imgTag1.setAttribute('loading', 'lazy');
+    spotlightCompany1.textContent = companyName1;
+    
+    imgTag2.setAttribute('src', imgURL2);
+    imgTag2.setAttribute('alt', `Profile of ${companyName2}`);
+    imgTag2.setAttribute('loading', 'lazy');
+    spotlightCompany2.textContent = companyName2;
 
-        displayMembers.appendChild(card);
+    spotlight1.appendChild(imgTag1);
+    spotlight2.appendChild(imgTag2);
 
-    });
+}
+
+function getRandomElements(array, numberOfElements) {
+  var shuffled = array.slice(0), i = array.length, min = i - numberOfElements, temp, index;
+  while (i-- > min) {
+    index = Math.floor((i + 1) * Math.random());
+    temp = shuffled[index];
+    shuffled[index] = shuffled[i];
+    shuffled[i] = temp;
+  }
+  return shuffled.slice(min);
 }
